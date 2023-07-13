f58c51c30345cd0c5976da9b27c108dbc03b4692
MarkSuttirak <suttirak.ch@zaviago.com>
2023-07-13 17:15:39 +0700

    Update redemption.jsx

--- honda/src/pages/points-rewards/redemption.jsx
+++ honda/src/pages/points-rewards/redemption.jsx
@@ -24,11 +24,12 @@ const Redemption = () => {
 
   let { id } = useParams()
 
-  const { data, isLoading, error, mutate } = useFrappeGetDoc('Item', id, {
+  const { data, isValidating, error, mutate } = useFrappeGetDoc('Item', id, {
     fields: ['item_name', 'item_group', 'image', 'points']
   })
 
   useFrappeDocumentEventListener('Item', (d) => {
+    console.log(d);
     if (d.doctype === 'Item'){
       mutate();
     }
@@ -38,40 +39,38 @@ const Redemption = () => {
     <div>
       <HeaderMobile firstIcon={faChevronLeft} title={(<img src={HondaNon} width="94px" style={{margin:"auto"}}/>)} secondBtn={false} />
       <main>
+        {isValidating ? (
+          <div className="brand-info p-6 pt-12">
+            <Skeleton />
+          </div>
+        ) : (
           <>
-            {isLoading ? (
-              <Skeleton />
-            ) : (
-              <>{data && (<img src={data.image} width="100%" style={{height:"280px"}} />)}</>
-            )}
-            <div className="brand-info p-6">
-              <div className="grid grid-cols-2 flex items-center justify-between">
-                <div className="block">
-                  <h5 className="text-xs">Testtype</h5>
-                  {isLoading ? (
-                    <Skeleton />
-                  ) : (
-                    <>{data && (<h2 className="font-bold text-xl">{data.item_name}</h2>)}</>
-                  )}
+          {data && 
+            <>
+              <img src={data.image} width="100%" style={{height:"280px"}} />
+              <div className="brand-info p-6">
+                <div className="grid grid-cols-2 flex items-center justify-between">
+                  <div className="block">
+                    <h5 className="text-xs">{data.item_group}</h5>
+                    <h2 className="font-bold text-xl">{data.item_name}</h2>
+                  </div>
                 </div>
-              </div>
 
-              <div className="grid grid-cols-2 mt-4 pb-2">
-                <div className="block pr-4" style={{borderRight:"1px solid #0000001A"}}>
-                  <p className="text-xs" style={{color:"#00000061"}}>คะแนนที่ใช้</p>
-                  {isLoading ? (
-                    <Skeleton />
-                  ) : (
-                    <>{data && (<h1 className="font-with-inter primary-color text-2xl font-semibold">{data.points} คะแนน</h1>)}</>
-                  )}
-                </div>
-                <div className="block pl-4">
-                  <p className="text-xs" style={{color:"#00000061"}}>คูปองมีอายุการใช้งานภายใน</p>
-                  <h1 className="font-with-inter primary-color text-md font-semibold">1 day</h1>
+                <div className="grid grid-cols-2 mt-4 pb-2">
+                  <div className="block pr-4" style={{borderRight:"1px solid #0000001A"}}>
+                    <p className="text-xs" style={{color:"#00000061"}}>คะแนนที่ใช้</p>
+                    <h1 className="font-with-inter primary-color text-2xl font-semibold">{data.points} คะแนน</h1>
+                  </div>
+                  <div className="block pl-4">
+                    <p className="text-xs" style={{color:"#00000061"}}>คูปองมีอายุการใช้งานภายใน</p>
+                    <h1 className="font-with-inter primary-color text-md font-semibold">1 day</h1>
+                  </div>
                 </div>
               </div>
-            </div>
+            </>
+          }
           </>
+        )}
       </main>
     </div>
   )
